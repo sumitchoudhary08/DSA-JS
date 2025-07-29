abad169b544d5b0c9936bd0012d546475d5c37c8
sumit <sumitbca93@gmail.com>
2025-07-29 15:17:26 +0530

    bubble sort

---
+++ SearchingAndSorting/bubbleSort.js
@@ -0,0 +1,22 @@
+const bubbleSort = (arr) => {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let swapped = false;
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return arr;
+};
+
+const arr = [1, 2, 5, 3, 9, 4, 7];
+
+console.log(bubbleSort(arr));
